f23b8b5ba05f16ef07dbe16b08be8dfd1864c934
Ludovic Volat <ludovic.volat@gmail.com>
2024-08-22 17:40:15 +0200

    feat: first attempt

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# CFM_data_2024
+# CFM_data_2024\ninit on Thu 22 Aug 2024 05:38:56 PM CEST
---
+++ init.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "\ninit on $(date)" >> README.md
+
+#init of the working folders
+mkdir -p data work submission figures
